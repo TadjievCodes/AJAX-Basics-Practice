8e66f52594fbfad17ba542d97884d3dd664174db
TadjievCodes <muhammad2008.08@mail.ru>
2020-05-22 09:26:33 -0400

    AJAX basics

---
+++ js/widget.js
@@ -0,0 +1,24 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var employees = JSON.parse(xhr.responseText);
+      console.log(employees);
+      var statusHTMl = '<ul class="bulleted">';
+      for(var i = 0; i < employees.length; i +=1){
+        if(employees[i].inoffice === true ){ 
+            statusHTMl += '<li class="in">';
+        } else {
+           statusHTMl += '<li class="out">';
+       }
+        statusHTMl += employees[i].name;
+        statusHTMl += '</li>';
+      }
+      
+       statusHTMl += '</ul>';
+       document.getElementById('employeeList').innerHTML = statusHTMl;
+    }
+};
+
+xhr.open('GET','data/employees.json');
+xhr.send();
